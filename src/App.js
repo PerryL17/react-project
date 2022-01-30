37ced306bbaa3118291d6a716cd40227c0c87cb0
PerryL17 <77135000+PerryL17@users.noreply.github.com>
2022-01-30 11:24:27 -0800

    lesson 1-2 completed

--- src/App.js
+++ src/App.js
@@ -1,18 +1,13 @@
 import React from "react";
-const todoList = [
-  { id: 1, title: "groceries" },
-  { id: 2, title: "clean house" },
-  { id: 3, title: "pay bills" },
-];
+import ToDoList from "./TodoList.js";
+import AddTodoForm from "./AddToDoForm.js";
+
 function App() {
   return (
     <div>
       <h1>To Do List</h1>
-      <ul>
-        {todoList.map((item) => (
-          <li key={item.id}>{item.title}</li>
-        ))}
-      </ul>
+      <AddTodoForm />
+      <ToDoList />
     </div>
   );
 }
